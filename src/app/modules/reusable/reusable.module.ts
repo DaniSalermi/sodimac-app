83649bd8975e5d00057a35a50659ab75c604c5a6
Daniela Salermi <salermi22@gmail.com>
2020-01-07 19:14:48 -0300

    fix router link

--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
   declarations: [NavigationBarComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   exports: [NavigationBarComponent]
 })
 export class ReusableModule {}
