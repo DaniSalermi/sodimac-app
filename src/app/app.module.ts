6fc5db2ee2ecdfd9372af84ff8cfdde7f44ebf20
Daniela Salermi <salermi22@gmail.com>
2020-01-07 16:35:47 -0300

    fix bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,14 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { environment } from "../environments/environment";
-import { MyPlanningModule } from "./modules/my-planning/my-planning.module";
 
+import { MyPlanningModule } from "./modules/my-planning/my-planning.module";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { ReusableModule } from "./modules/reusable/reusable.module";
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    MyPlanningModule,
-    AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFirestoreModule
-  ],
+  imports: [BrowserModule, MyPlanningModule, AppRoutingModule, ReusableModule],
   providers: [],
   bootstrap: [AppComponent]
 })
