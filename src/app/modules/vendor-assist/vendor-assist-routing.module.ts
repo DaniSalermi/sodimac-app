97a9d65da22cdce644ae86c10825610f2c512e94
Nata Galvez <natagalvez>
2020-01-06 22:26:34 -0300

    imported component in the routing

--- src/app/modules/vendor-assist/vendor-assist-routing.module.ts
+++ src/app/modules/vendor-assist/vendor-assist-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule, Component } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AssistantComponent } from "./assistant/assistant.component";
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "asistencia",
+    component: AssistantComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class VendorAssistRoutingModule { }
+export class VendorAssistRoutingModule {}
