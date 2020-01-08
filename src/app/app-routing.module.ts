4363c1a221b034c09cd38be71db15a12707b6368
nigmasilmi <nigmasilmi.web@gmail.com>
2020-01-08 20:43:10 -0300

    restauradas comillas dobles en el app.routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,40 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
   {
-        path: 'home',
+        path: "home",
         loadChildren: () =>
-          import('./modules/home/home.module').then(mod => mod.HomeModule)
+          import("./modules/home/home.module").then(mod => mod.HomeModule)
   },
   {
-    path: 'planning',
+    path: "planning",
     loadChildren: () =>
-      import('./modules/my-planning/my-planning.module').then(
+      import("./modules/my-planning/my-planning.module").then(
         mod => mod.MyPlanningModule
       )
   },
 
   // path para entrar a la vista de desarrollo de productos temporal
   {
-    path: 'samplecat',
+    path: "samplecat",
     loadChildren: () =>
-      import('./modules/catalogue/catalogue.module').then(
+      import("./modules/catalogue/catalogue.module").then(
         mod => mod.CatalogueModule
       )
   },
   // fin del path temporal
   {
-    path: 'assistant',
+    path: "assistant",
     loadChildren: () =>
-      import('./modules/vendor-assist/vendor-assist.module').then(
+      import("./modules/vendor-assist/vendor-assist.module").then(
         mod => mod.VendorAssistModule
       )
   },
   {
-    path: 'welcome',
+    path: "welcome",
     loadChildren: () =>
-      import('./modules/pre-home/pre-home.module').then(
+      import("./modules/pre-home/pre-home.module").then(
         mod => mod.PreHomeModule
       )
   }
