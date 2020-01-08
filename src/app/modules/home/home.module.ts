65da8fc3f3fece25bc70fd013708914f549121fd
Nata Galvez <natagalvez>
2020-01-08 12:18:23 -0300

    add tag app-navigation-bar

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,22 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { HomeRoutingModule } from './home-routing.module';
-import { SodimacHomeComponent } from './sodimac-home/sodimac-home.component';
-import { OwlModule } from 'ngx-owl-carousel';
-import { HeaderComponent } from './header/header.component';
+import { HomeRoutingModule } from "./home-routing.module";
+import { SodimacHomeComponent } from "./sodimac-home/sodimac-home.component";
+import { OwlModule } from "ngx-owl-carousel";
+import { HeaderComponent } from "./header/header.component";
+import { ReusableModule } from "../reusable/reusable.module";
 
 @NgModule({
   declarations: [SodimacHomeComponent, HeaderComponent],
-  imports: [
-    CommonModule,
-    HomeRoutingModule,
-    OwlModule
-  ],
-  exports: [
-    SodimacHomeComponent
-  ],
+  imports: [CommonModule, HomeRoutingModule, OwlModule, ReusableModule],
+  exports: [SodimacHomeComponent],
   providers: [],
   bootstrap: [SodimacHomeComponent]
 })
-export class HomeModule { }
+export class HomeModule {}
