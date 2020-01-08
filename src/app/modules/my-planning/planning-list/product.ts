97db32e49919717d233d8550b89f778ecb8e58b6
nigmasilmi <nigmasilmi.web@gmail.com>
2020-01-08 11:27:48 -0300

    partially functioning planning lists -- uploading for team database management references

---
+++ src/app/modules/my-planning/planning-list/product.ts
@@ -0,0 +1,10 @@
+export interface Product {
+    area: string;
+    branch: string;
+    brand: string;
+    category: string;
+    id: string;
+    name: string;
+    price: number;
+    sku: string;
+}
