c1fdc49c0ba5ee53eedc866898b4dcd93b543462
Nata Galvez <natagalvez>
2020-01-09 00:46:42 -0300

    template product-detail okey!

--- src/app/modules/products-detail/products-detail.module.ts
+++ src/app/modules/products-detail/products-detail.module.ts
@@ -5,10 +5,16 @@ import { ProductsDetailRoutingModule } from "./products-detail-routing.module";
 import { DetailsComponent } from "./details/details.component";
 import { HeaderComponent } from "../home/header/header.component";
 import { HomeModule } from "../home/home.module";
+import { ReusableModule } from "../reusable/reusable.module";
 
 @NgModule({
   declarations: [DetailsComponent],
-  imports: [CommonModule, ProductsDetailRoutingModule, HomeModule],
+  imports: [
+    CommonModule,
+    ProductsDetailRoutingModule,
+    HomeModule,
+    ReusableModule
+  ],
   exports: [DetailsComponent]
 })
 export class ProductsDetailModule {}
