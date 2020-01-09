f0a83628ce6eaa33f9ec52beac750dae59b77de8
Nata Galvez <natagalvez>
2020-01-08 23:59:25 -0300

    z

--- src/app/services/my-planning/my-planning-crud.service.ts
+++ src/app/services/my-planning/my-planning-crud.service.ts
@@ -1,32 +1,32 @@
-import { Injectable } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { map } from 'rxjs/operators';
-import { Product } from '../../modules/my-planning/planning-list/product';
+import { Injectable } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { map } from "rxjs/operators";
+import { Product } from "../../modules/my-planning/planning-list/product";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class MyPlanningCrudService {
-
   products: any;
   plannings: any;
   productListArr = [];
 
-  constructor(public afs: AngularFirestore) { }
+  constructor(public afs: AngularFirestore) {}
   // CREATE PRODUCTS AND LISTS //
 
   // add product to products collection
   // development only
   addProduct() {
     const dataToSave = {
-      area: 'Construcción y Reparación',
-      branch: 'Gasfitería',
-      brand: 'Fas',
-      category: 'Griferías',
-      img: 'https://res.cloudinary.com/dz3gm9c3w/image/upload/v1578410785/hackathon-sodimac/3598861b_u67as5.png',
-      name: 'Monomando Lavaplatos Línea Galia Plus',
+      area: "Construcción y Reparación",
+      branch: "Gasfitería",
+      brand: "Fas",
+      category: "Griferías",
+      img:
+        "https://res.cloudinary.com/dz3gm9c3w/image/upload/v1578410785/hackathon-sodimac/3598861b_u67as5.png",
+      name: "Monomando Lavaplatos Línea Galia Plus",
       price: 34990,
-      sku: '359886-1',
+      sku: "359886-1",
       mockRow: 3
     };
     this.saveProduct(dataToSave);
@@ -35,26 +35,27 @@ export class MyPlanningCrudService {
   saveProduct(data) {
     return new Promise<any>((resolve, reject) => {
       this.afs
-        .collection('products')
+        .collection("products")
         .add(data)
-        .then(res => {
-          console.log('data saved');
-        },
-          err => reject(err));
+        .then(
+          res => {
+            console.log("data saved");
+          },
+          err => reject(err)
+        );
     });
   }
 
   // end of the addProduct functionality for development only
-
 
   // add list to planList collection
   // development only
 
   addPlanList() {
     const dataToSave = {
-      plan_name: 'Remodelar la cocina',
-      products: ['372559-6', '540235-2'],
-      store: 'Ñuble'
+      plan_name: "Remodelar la cocina",
+      products: ["372559-6", "540235-2"],
+      store: "Ñuble"
     };
     this.saveList(dataToSave);
   }
@@ -62,32 +63,36 @@ export class MyPlanningCrudService {
   saveList(data) {
     return new Promise<any>((resolve, reject) => {
       this.afs
-        .collection('planningLists')
+        .collection("planningLists")
         .add(data)
-        .then(res => {
-          console.log('planning saved');
-        },
-          err => reject(err));
+        .then(
+          res => {
+            console.log("planning saved");
+          },
+          err => reject(err)
+        );
     });
   }
 
   // end of the addPlanList functionality for development only
-
 
   // RETRIEVE OR READ PRODUCTS AND LISTS
 
   // get products list
 
   getProducts() {
-    return this.products = this.afs.collection('products', ref => ref.orderBy('name', 'asc')).snapshotChanges();
+    return (this.products = this.afs
+      .collection("products", ref => ref.orderBy("name", "asc"))
+      .snapshotChanges());
   }
 
   // get planning lists
 
   getPlannings() {
-    return this.plannings = this.afs.collection('planningLists', ref => ref.orderBy('plan_name', 'asc')).snapshotChanges();
+    return (this.plannings = this.afs
+      .collection("planningLists", ref => ref.orderBy("plan_name", "asc"))
+      .snapshotChanges());
   }
-
 
   // get single product dejar comentado por favor
 
@@ -109,11 +114,6 @@ export class MyPlanningCrudService {
   // get single list
 
   getList(id) {
-    return this.afs.collection('productLists').doc(id);
+    return this.afs.collection("productLists").doc(id);
   }
-
-
-
-
 }
-
