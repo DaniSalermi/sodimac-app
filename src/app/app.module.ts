1f45178ef9da818cb01a06e361c99489a3081eb0
Daniela Salermi <51088476+DaniSalermi@users.noreply.github.com>
2020-01-09 14:33:03 -0300

    feature add styles, notification config with firebase cloud functions. (#15)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { VendorAssistModule } from './modules/vendor-assist/vendor-assist.module';
-import { PreHomeModule } from './modules/pre-home/pre-home.module';
-import { AngularFireModule } from '@angular/fire';
-import { AngularFirestoreModule } from '@angular/fire/firestore';
-import { environment } from '../environments/environment';
-import { MyPlanningModule } from './modules/my-planning/my-planning.module';
-import { HomeModule } from './modules/home/home.module';
-import { ReusableModule } from './modules/reusable/reusable.module';
-import { LocationModule } from './modules/location/location.module';
-
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { VendorAssistModule } from "./modules/vendor-assist/vendor-assist.module";
+import { PreHomeModule } from "./modules/pre-home/pre-home.module";
+import { AngularFireModule } from "@angular/fire";
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { environment } from "../environments/environment";
+import { MyPlanningModule } from "./modules/my-planning/my-planning.module";
+import { HomeModule } from "./modules/home/home.module";
+import { ReusableModule } from "./modules/reusable/reusable.module";
+import { AngularFireMessagingModule } from "@angular/fire/messaging";
+import { LocationModule } from "./modules/location/location.module";
 
 @NgModule({
   declarations: [AppComponent],
@@ -28,6 +28,7 @@ import { LocationModule } from './modules/location/location.module';
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
     ReusableModule,
+    AngularFireMessagingModule,
     LocationModule
   ],
   providers: [],
--- src/app/modules/vendor-assist/assistant/assistant.component.ts
+++ src/app/modules/vendor-assist/assistant/assistant.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { WaitlistService } from "src/app/services/waitlist.service";
 import { FormControl } from "@angular/forms";
 
 @Component({
@@ -7,18 +8,34 @@ import { FormControl } from "@angular/forms";
   styleUrls: ["./assistant.component.scss"]
 })
 export class AssistantComponent implements OnInit {
-  hallNumber = new FormControl();
-  numberInput: number;
-  name = new FormControl();
-  nameInput: string;
+  hall = new FormControl("");
+  clientName = new FormControl("");
+  halls: any = [];
 
-  constructor() {}
+  constructor(private waitlist: WaitlistService) {}
 
-  ngOnInit() {}
-  btnSale() {
-    this.numberInput = this.hallNumber.value;
-    console.log(this.numberInput);
-    this.nameInput = this.name.value;
-    console.log(this.nameInput);
+  ngOnInit() {
+    this.waitlist.getHallName().subscribe((store: any) => {
+      console.log(store[0]);
+      this.halls = store[0].halls;
+      this.hallName();
+    });
+  }
+
+  hallName() {
+    let output = this.halls.filter(
+      hall => hall.number === parseInt(this.hall.value)
+    );
+    console.log(output[0]);
+    return output[0];
+  }
+
+  saveClient() {
+    let halldata = this.hallName();
+    this.waitlist.addToWailist(
+      halldata.name,
+      halldata.number,
+      this.clientName.value
+    );
   }
 }
