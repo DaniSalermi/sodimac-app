2ae7791d1cb2456fcb270d7d579fdbc430d017aa
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2020-01-09 09:01:37 -0300

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { VendorAssistModule } from "./modules/vendor-assist/vendor-assist.module";
-import { PreHomeModule } from "./modules/pre-home/pre-home.module";
-import { AngularFireModule } from "@angular/fire";
-import { AngularFirestoreModule } from "@angular/fire/firestore";
-import { environment } from "../environments/environment";
-import { MyPlanningModule } from "./modules/my-planning/my-planning.module";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { VendorAssistModule } from './modules/vendor-assist/vendor-assist.module';
+import { PreHomeModule } from './modules/pre-home/pre-home.module';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+import { MyPlanningModule } from './modules/my-planning/my-planning.module';
 import { HomeModule } from './modules/home/home.module';
-import { ReusableModule } from "./modules/reusable/reusable.module";
+import { ReusableModule } from './modules/reusable/reusable.module';
+import { LocationModule } from './modules/location/location.module';
+
 
 @NgModule({
   declarations: [AppComponent],
@@ -25,7 +27,8 @@ import { ReusableModule } from "./modules/reusable/reusable.module";
     HomeModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    ReusableModule
+    ReusableModule,
+    LocationModule
   ],
   providers: [],
   bootstrap: [AppComponent]
