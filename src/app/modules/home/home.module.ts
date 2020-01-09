f745188f6cb8a87cc3705bac487f11e3e51aed33
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2020-01-09 09:50:59 -0300

    location icon position

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-import { HomeRoutingModule } from "./home-routing.module";
-import { SodimacHomeComponent } from "./sodimac-home/sodimac-home.component";
-import { OwlModule } from "ngx-owl-carousel";
-import { ReusableModule } from "../reusable/reusable.module";
+import { HomeRoutingModule } from './home-routing.module';
+import { SodimacHomeComponent } from './sodimac-home/sodimac-home.component';
+import { OwlModule } from 'ngx-owl-carousel';
+import { ReusableModule } from '../reusable/reusable.module';
 
 @NgModule({
   declarations: [SodimacHomeComponent],
