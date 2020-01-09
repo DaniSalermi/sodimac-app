dea18ec80c9905220faab6bdba06255c5601aeae
ClaudiaQuezadaRiquelme <cklaho.quesoda.X@gmail.com>
2020-01-09 09:15:07 -0300

    header moved to reusable

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -4,13 +4,12 @@ import { CommonModule } from "@angular/common";
 import { HomeRoutingModule } from "./home-routing.module";
 import { SodimacHomeComponent } from "./sodimac-home/sodimac-home.component";
 import { OwlModule } from "ngx-owl-carousel";
-import { HeaderComponent } from "./header/header.component";
 import { ReusableModule } from "../reusable/reusable.module";
 
 @NgModule({
-  declarations: [SodimacHomeComponent, HeaderComponent],
+  declarations: [SodimacHomeComponent],
   imports: [CommonModule, HomeRoutingModule, OwlModule, ReusableModule],
-  exports: [SodimacHomeComponent, HeaderComponent],
+  exports: [SodimacHomeComponent],
   providers: [],
   bootstrap: [SodimacHomeComponent]
 })
--- src/app/modules/reusable/reusable.module.ts
+++ src/app/modules/reusable/reusable.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { NavigationBarComponent } from "./navigation-bar/navigation-bar.component";
-import { FooterComponent } from "./footer/footer.component";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [NavigationBarComponent, FooterComponent],
+  declarations: [NavigationBarComponent, FooterComponent, HeaderComponent],
   imports: [CommonModule, RouterModule],
-  exports: [NavigationBarComponent, FooterComponent]
+  exports: [NavigationBarComponent, FooterComponent, HeaderComponent]
 })
 export class ReusableModule {}
